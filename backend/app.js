6140f2d2ef869ea281488813a281d49299d36a81
azim128 <azimruet28@gmail.com>
2024-06-06 01:25:11 +0600

    feat2: add response handler, not found handler and global error handler

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,8 @@
 import dotenv from "dotenv";
 import express from "express";
 import connectDB from "./config/db_config.js";
+import { AppError, globalErrorHandler, notFoundHandler } from "./middlewares/errorHandlers.middleware.js";
+import { error, success } from "./middlewares/responseHandler.middleware.js";
 dotenv.config();
 
 const app = express();
@@ -8,6 +10,10 @@ const app = express();
 // middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+
+// add router
+
 
 // app.post("/send-email", async (req, res) => {
 //   const { email } = req.body;
@@ -23,19 +29,20 @@ app.use(express.urlencoded({ extended: true }));
 //     res.send("Failed to send email!"); // Moved this line inside the else block
 //   }
 // });
+// Example route that triggers an error
 
 app.get("/health", (req, res) => {
-  res.send("OK!");
+  success(res, "Server Running", "OK!" );
 });
 
-// global error handler if any middleware throws an error
-app.use((err, req, res, next) => {
-  if (err.status) {
-    res.status(err.status).send(err.message);
-  } else {
-    res.status(500).send("Something broke!");
-  }
-});
+
+// Not found handler
+app.use(notFoundHandler);
+
+// Global error handler
+app.use(globalErrorHandler);
+
+
 const port = 3000;
 // if db connect then run the server
 connectDB()
